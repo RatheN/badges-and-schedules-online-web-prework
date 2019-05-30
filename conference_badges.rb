2f4945534367ea83a4e3fa52820011e16efc922d
Nathan Rathe <nprathe10@gmail.com>
2019-05-30 02:23:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,6 @@ end
 
 def batch_badge_creator(names)
   names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  names.each do |name|
-    puts "Hello, my name is #{name}."
+  names.each do { |name| puts "Hello, my name is #{name}." }
   end
 end
